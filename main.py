f63b5659915600ac2ba387b0ac52c276b9867d0d
Ffdsfd <78157933+Ffdsfd@users.noreply.github.com>
2022-04-06 09:30:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.run()
+    if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 @login_manager.user_loader
